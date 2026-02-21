57e59439d9438775cedff16584d5eb6c8bf4fb2f
Stanislav Popov <popstas@gmail.com>
2026-02-21 23:12:04 +0500

    release: v0.1.2

--- keychron-via-hue/src/keychron_via_hue/__init__.py
+++ keychron-via-hue/src/keychron_via_hue/__init__.py
@@ -1,6 +1,6 @@
 """keychron-via-hue - CLI utility for managing RGB hue of QMK/VIA-compatible keyboards."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
 
--- src/rgb_keyboard_language_windows/__init__.py
+++ src/rgb_keyboard_language_windows/__init__.py
@@ -1,4 +1,4 @@
 """RGB Keyboard Language - Windows tray application for automatic keyboard RGB color based on layout."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
